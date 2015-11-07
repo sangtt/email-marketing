3ff478f6041314b628301dc9f91d9dbf98937390
Viet. Truong <truongthanhviet1991@gmail.com>
2015-11-07 11:55:42 +0700

    feat(mailer): Setup mailer
    Basic sending email

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 'use strict';
 
+var emailMarketing = require('./lib/email-marketing');
+
+emailMarketing('/Users/sangtruong/code/email-marketing/templates/emails');
+
 module.exports = require('./lib/email-marketing');
